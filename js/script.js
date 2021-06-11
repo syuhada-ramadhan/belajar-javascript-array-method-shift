be8da17d54af4d29aeb5c2e22191a2241a3a25ed
syuhada-ramadhan <suhadahada18@gmail.com>
2021-06-11 16:07:44 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// array hewan
+const hewan = ["Kambing", "Kelinci"];
+
+// function untuk menghapus elemen pertama array hewan
+const hapusElemenPertama = () => hewan.shift();
+
+// panggil function hapusElemenPertama()
+hapusElemenPertama();
+
+// cetak array hewan
+console.log(hewan);
